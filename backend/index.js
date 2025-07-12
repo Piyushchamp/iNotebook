adab63a8c809ceba0d86c8dab33a4ebca83a9e51
Piyushchamp <113053000+Piyushchamp@users.noreply.github.com>
2025-07-12 20:19:12 +0530

    c

--- backend/index.js
+++ backend/index.js
@@ -15,13 +15,17 @@ app.use(cors({
 
 app.use(express.json());
 
-//Available Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
 
 app.get("/", (req, res) => {
   res.send("hii");
 });
+
+
+//Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+
 
 
 app.listen(port, () => {
